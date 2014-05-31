7a4eec60e8553f418c0bb743f5fbc64bd906739b
Paul Melliere <pmelliere@gmail.com>
2014-05-31 18:52:36 -0400

    updated location to current location

--- client/scripts/controllers/areaController.js
+++ client/scripts/controllers/areaController.js
@@ -4,24 +4,35 @@ var controllersModule = angular.module('controllersModule');
 controllersModule.controller('areaController', ['$scope', 'Auth', '$location', '$http',
     function ($scope, Auth, $location, $http) {
 
+       $scope.mapRefresh = false;
+       $scope.yourLocation = {};
+       $scope.reports = {};
+        //latitude: 39.848829699999996,
+            //longitude: -86.14148809999999
+        //$scope.map = {};
         $scope.map = {
             center: {
-                latitude: 45,
-                longitude: -73
+                latitude: 3,
+                longitude: -8
             },
-            zoom: 16
+            zoom: 16,
+            control: {}
         };
 
         function setPosition(position) {
             console.log('got position' + JSON.stringify(position));
-            $scope.map = {
-                center: {
+            /*$scope.map.
+                center = {
                     latitude: position.coords.latitude,
                     longitude: position.coords.longitude
-                },
-                zoom: 16
-            }
-            $scope.map.refresh();
+                }
+*/
+             // zoom: 16
+            //}
+            //$scope.map.mapRefresh = true;
+            $scope.yourLocation = position.coords;
+            $scope.map.control.refresh({latitude: position.coords.latitude, longitude: position.coords.longitude});
+
         }
 
         if (navigator.geolocation) {
@@ -34,3 +45,8 @@ controllersModule.controller('areaController', ['$scope', 'Auth', '$location', '
 
         }]
         );
+
+        $http.get('/reports')
+.success(function(data) {
+$scope.reports = data;
+            });
